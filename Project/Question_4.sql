b4c1e9eab55ec3ebeb7a4a40c7f92d2fbc076b84
Prajwal01406 <prajwal2006@hotmail.com>
2024-12-04 22:26:39 +0530

    q4

--- Project/Question_4.sql
+++ Project/Question_4.sql
@@ -20,3 +20,21 @@ WHERE
     AND payment_mode != 'Cash'
 
 GROUP BY payment_mode;
+
+/* OR */ 
+
+SELECT
+    COUNT( CASE WHEN gender = 'Male' THEN 1  END) AS Male,
+    COUNT( CASE WHEN gender = 'Female' THEN 1  END) AS Female,
+    payment_mode,
+    SUM(price) AS Amt_spent
+
+FROM
+    customer_data CD JOIN sales_data SD
+    ON CD.customer_id = SD.customer_id
+
+WHERE 
+    category = 'Food & Beverage'
+    AND payment_mode != 'Cash'
+
+GROUP BY payment_mode;
